57d6484111aaba3e05b2e475f7afd57c35d42ac4
anobee <HP@DESKTOP-KFAVCD8>
2024-05-07 15:51:46 +0700

    p

--- 4.cpp
+++ 4.cpp
@@ -1,5 +1,46 @@
 #include <iostream>
 using namespace std;
+include string;
+
 
 class siswa;
-cass orang;
+class orang;
+{
+    private:
+        string nama;
+    public:
+        void setnama(string pnama);
+        friend class siswa;
+};
+
+class siswa
+{
+    private:
+    int id;
+
+    public:
+        void setid(int pId);
+        void displayall (orang &a);
+};
+
+void siswa::displayall(orangtua &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setnama (string pnama)
+{
+    nama = pnama;
+}
+void siswa::setid(int pid)
+{
+    id = pId;
+}
+int main ()
+{
+    orang joko;
+    joko.setnama("joko WI");
+    siswa joko_siswa;
+    joko_siswa setid(1)
+    joko_siswa.diplayall(joko);
+    return 0;
+}
